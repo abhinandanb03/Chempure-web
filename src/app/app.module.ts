3b37ddc13b500d8a23fb6e37414f2bf063497502
abhinandanb03 <abhinandanb03@gmmail.com>
2021-05-13 09:24:10 +0530

    updated home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
   img1 = 'assets/img/home/carousel1.png';
   img2 = 'assets/img/home/carousel2.png';
@@ -17,11 +19,73 @@ export class HomeComponent implements OnInit {
   bg1 = 'assets/img/home/bg1.gif';
   bg2 = 'assets/img/home/bg2.gif';
   bg3 = 'assets/img/home/bg3.gif';
+  
+  slides = [
+    {id: 1, img: 'assets/img/clients/c1.jpg'},
+    {id: 2, img: 'assets/img/clients/c2.jpg'},
+    {id: 3, img: 'assets/img/clients/c3.jpg'},
+    {id: 4, img: 'assets/img/clients/c4.jpg'},
+    {id: 5, img: 'assets/img/clients/c5.jpg'},
+    {id: 6, img: 'assets/img/clients/c6.jpg'},
+    {id: 7, img: 'assets/img/clients/c7.jpg'},
+    {id: 8, img: 'assets/img/clients/c8.jpg'},
+    {id: 9, img: 'assets/img/clients/c9.png'},
+    {id: 10, img: 'assets/img/clients/c10.jpg'},
+    {id: 11, img: 'assets/img/clients/c11.jpg'},
+    {id: 12, img: 'assets/img/clients/c12.jpg'}
+  ];
 
-  hislide = 'assets/jquery.hislide.js'
+  card = [{
+    img: 'assets/img/home/iso-logo.jpg',
+    txt: 'ISO 9001 : 2015 Certified Since 2004',
+  },
+  {
+    img: 'assets/img/home/iso-logo1.jpg',
+    txt: 'NSF Certified Since 2019'
+  },
+  {
+    img: 'assets/img/home/iso-logo2.jpg',
+    txt: 'NSIC Certified Since 2012',
+  },
+  {
+    img: 'assets/img/home/iso-logo4.jpg',
+    txt: 'Member of IWO Since 2008'
+  },
+  {
+    img: 'assets/img/home/NFS-logo.jpg',
+    txt: 'Member of IWA since 2003 - Young Professional Development Award 2008'
+  },
+  {
+    img: 'assets/img/home/NSIC-logo.jpg',
+    txt: 'Business Excellence Award for the year 2008 from International Study Circle'
+  }]
+
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: true,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 500,
+    navText: ['', ''],
+    margin: 10,
+    responsive: {
+      0:{
+        items:3
+      },
+      600:{
+          items:5
+      },
+      1000:{
+          items:6
+      }
+      },
+    nav: false,
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
