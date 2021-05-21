4703401a1f6e69e82b55a5290f4ceeb65f78d43a
abhinandanb03 <abhinandanb03@gmmail.com>
2021-05-21 18:55:09 +0530

    updated wts

--- src/app/products/waste/waste.component.ts
+++ src/app/products/waste/waste.component.ts
@@ -19,7 +19,23 @@ export class WasteComponent implements OnInit {
   img10= 'assets/img/products/wwt/img10.jpg'
   img11= 'assets/img/products/wwt/img11.jpg'
   img12= 'assets/img/products/wwt/img12.png'
-  
+  img13= 'assets/img/products/wwt/img13.jpg'
+  img14= 'assets/img/products/wwt/img14.jpg'
+  img15= 'assets/img/products/wwt/img16.png'
+  img16=  'assets/img/products/wwt/img17.png'
+  img17=  'assets/img/products/wwt/img18.png'
+  img18= 'assets/img/products/wwt/img19.png'
+  img19= 'assets/img/products/wwt/img20.png'
+  img20= 'assets/img/products/wwt/img21.png'
+  img21= 'assets/img/products/wwt/img22.png'
+  img22= 'assets/img/products/wwt/img23.png'
+  img23= 'assets/img/products/wwt/img15.jpg'
+  img24= 'assets/img/products/wwt/img25.png'
+  img25= 'assets/img/products/wwt/img26.png'
+  img26= 'assets/img/products/wwt/img27.png'
+  img27= 'assets/img/products/wwt/img28.jpg'
+  img28= 'assets/img/products/wwt/img29.png'
+
   constructor() { }
 
   ngOnInit(): void {
