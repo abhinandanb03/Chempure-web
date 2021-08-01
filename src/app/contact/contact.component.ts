25708d6c9bf30e4983775455593954b0d26c1a90
Anantha <vishal.vijay016@gmail.com>
2021-08-01 22:34:09 +0530

    added email sender

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  cover = 'assets/img/contact/cover.png'
-  constructor() { }
+  cover = 'assets/img/contact/cover.png';
+  contact = {
+    name: '',
+    email: '',
+    subject:'',
+    phone: 0,
+    message: '',
+
+  }
+  constructor()  {
+    
+   }
 
   ngOnInit(): void {
+    
   }
+   
+ async sendEmail(e: Event){
+  emailjs.sendForm('service_016t1rt', 'template_hffn4a7',e.target as HTMLFormElement,'user_MiXV2QqwHfyZOuy6KCZjg')
+  .then((result: EmailJSResponseStatus) => {
+    console.log(result.text);
+  }, (error) => {
+    console.log(error.text);
+  });
 
+  }
 }
